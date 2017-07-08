13f5f182a0c7280e9395134faf42dc9b3842c662
venkatrajulapati <venkata.rajulapati@gmail.com>
2017-07-08 16:17:15 +0530

    first commit

---
+++ firstprogram.py
@@ -0,0 +1,25 @@
+import logging
+class FamilyInformation:
+
+    def __init__(self):
+
+        self._surname = "rajulapati"
+
+
+    def AddParentInfo(self,FatherName,MotherName):
+
+        self.father = FatherName + self._surname
+        self.Mother = MotherName + self._surname
+
+
+
+
+obj = FamilyInformation()
+obj.AddParentInfo("Subbarao","Prabhavathi devi")
+print((obj.father))
+print(obj.Mother)
+print(obj._surname)
+#logging.basicConfig(filename="C:\\Users\\RAJULAPATI\\Desktop\\python docs\\docs-pdf\\outputlog.log",filemode='w',level=logging.DEBUG)
+#logging.info("Log message")
+logger = logging.getLogger(logging.basicConfig(filename="C:\\Users\\RAJULAPATI\\Desktop\\python docs\\docs-pdf\\outputlog.log",filemode='w',level=logging.DEBUG))
+logger.info("log new message")
